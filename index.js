22f5206fc90d540d3ddcfc59b587e5aa0a74b1b3
christian-gerard <1@christiangerard.me>
2024-01-29 16:55:05 -0700

    FINAL

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(dist) {
+    return Math.abs(dist - 42)
+}
+
+function distanceFromHqInFeet (dist) {
+    return Math.abs((dist - 42) * 264)
+}
+
+function distanceTravelledInFeet (start, stop) {
+    return Math.abs((start - stop) * 264)
+}
+
+function calculatesFarePrice (start, destination) {
+    const dist = Math.abs(start - destination)
+    const feetTravelled = dist * 264;
+
+    if (feetTravelled <= 400) {
+        return 0
+    } else if (feetTravelled <= 2000) {
+        return ((feetTravelled - 400) * 0.02)
+    } else if (feetTravelled < 2500) {
+        return 25
+    } else {
+        return "cannot travel that far"
+    }
+    
+}
